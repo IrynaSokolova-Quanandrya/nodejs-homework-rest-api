65d300260bd42f087e5a37d0fea42332c8d3b2f1
Iryna <irasokolova25@ukr.net>
2022-01-29 00:11:54 +0200

    add mongoose, dotenv and connect to DB

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+require('dotenv').config()
+const {DB_HOST} = process.env;
+require('mongoose').connect(DB_HOST)
+.then(()=>console.log("Database connection successful"))
+.catch(error=>console.log(error.message))
+// process.exit(1)
 
 const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
-
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+// PZoVrUvZro7OgW3O
+// mongodb+srv://ira_sokolova:PZoVrUvZro7OgW3O@cluster0.81849.mongodb.net/test
+// mongodb+srv://ira_sokolova:<password>@cluster0.81849.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 const app = require('./app')
-
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const {DB_HOST, PORT = 3000} = process.env;
+require('mongoose').connect(DB_HOST)
+.then(()=>app.listen(PORT, () => {
+  console.log("Database connection successful")
+}))
+.catch(error=>{
+  console.log(error.message)
+  process.exit(1)
 })
+ 
+
+
