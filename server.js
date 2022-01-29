120f9a74b1f9ab67ed316cf1aa683e0068cd677c
Iryna <irasokolova25@ukr.net>
2022-01-29 16:11:57 +0200

    delete comments

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-// PZoVrUvZro7OgW3O
-// mongodb+srv://ira_sokolova:PZoVrUvZro7OgW3O@cluster0.81849.mongodb.net/test
-// mongodb+srv://ira_sokolova:<password>@cluster0.81849.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 const app = require('./app')
 const {DB_HOST, PORT = 3000} = process.env;
 
